50f3444a25280911a2a1afca1d9456d157110f3a
Narek <narek1994t@gmail.com>
2021-05-13 16:29:23 +0400

    some fixes

--- app/src/main/java/com/vuukle/webview/manager/url/UrlManager.kt
+++ app/src/main/java/com/vuukle/webview/manager/url/UrlManager.kt
@@ -19,6 +19,6 @@ class UrlManager(activity: AppCompatActivity) {
     }
 
     fun getPowerBarUrl(): String {
-        return "https://cdn.vuukle.com/widgets/powerbar.html?amp=false&apiKey=664e0b85-5b2c-4881-ba64-3aa9f992d01c&host=relaxed-beaver-76304e.netlify.com&articleId=Index&img=https%3A%2F%2Fwww.gettyimages.ie%2Fgi-resources%2Fimages%2FHomepage%2FHero%2FUK%2FCMS_Creative_164657191_Kingfisher.jpg&title=Index&url=https%3A%2F%2Frelaxed-beaver-76304e.netlify.app%2F&tags=123&author=123&lang=en&gr=false&darkMode=false&defaultEmote=1&items=&standalone=0&mode=horizontal"
+        return "https://cdn.vuukle.com/amp-sharebar.html?amp=false&apiKey=664e0b85-5b2c-4881-ba64-3aa9f992d01c&host=relaxed-beaver-76304e.netlify.com&id=Index&img=https%3A%2F%2Fwww.gettyimages.ie%2Fgi-resources%2Fimages%2FHomepage%2FHero%2FUK%2FCMS_Creative_164657191_Kingfisher.jpg&title=Index&url=https%3A%2F%2Frelaxed-beaver-76304e.netlify.app%2F&tags=123&author=123&lang=en&gr=false&darkMode=false&defaultEmote=1&items=&mode=horizontal&comments=true"
     }
 }
